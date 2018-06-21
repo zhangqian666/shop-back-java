44b90dcfad4075b9da29b668068fd78fee4921ca
mac <415677303@qq.com>
2018-06-21 22:41:37 +0800

    修改bug

--- example/src/main/java/com/zq/shop/web/service/impl/FileServiceImpl.java
+++ example/src/main/java/com/zq/shop/web/service/impl/FileServiceImpl.java
@@ -82,7 +82,7 @@ public class FileServiceImpl implements IFileService {
 
     private void aliUpFile(File file, String filename) throws IOException {
         // endpoint以杭州为例，其它region请按实际情况填写。
-        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
+        String endpoint = "https://oss-cn-beijing-internal.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
         String accessKeyId = "LTAI0KjgIKFUwRfz";
         String accessKeySecret = "yOWp1p0kuMLAQSCdU5q3NevDea0FXs";
