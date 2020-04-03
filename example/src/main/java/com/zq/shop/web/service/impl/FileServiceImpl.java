88b6ac55dbe26127cfe261e052bd118e905722d3
张迁 <18310623602@163.com>
2020-04-03 17:03:31 +0800

    Update FileServiceImpl.java

--- example/src/main/java/com/zq/shop/web/service/impl/FileServiceImpl.java
+++ example/src/main/java/com/zq/shop/web/service/impl/FileServiceImpl.java
@@ -86,8 +86,8 @@ public class FileServiceImpl implements IFileService {
         // endpoint以杭州为例，其它region请按实际情况填写。
         String endpoint = "https://oss-cn-beijing.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI0KjgIKFUwRfz";
-        String accessKeySecret = "yOWp1p0kuMLAQSCdU5q3NevDea0FXs";
+        String accessKeyId = "";
+        String accessKeySecret = "";
         // 创建OSSClient实例。
         ClientBuilderConfiguration conf = new ClientBuilderConfiguration();
         conf.setConnectionTimeout(5000);
